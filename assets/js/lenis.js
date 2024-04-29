47b3f19cccfec5290325fe11949292e9c3b74ae8
Julio Coronetti <juliodevsenai@gmail.com>
2024-04-29 13:11:12 -0300

    first commit

---
+++ assets/js/lenis.js
@@ -0,0 +1,27 @@
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            lenis.scrollTo(target, {
+                duration: 2
+            });
+        }
+    });
+});
